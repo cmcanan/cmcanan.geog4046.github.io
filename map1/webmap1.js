b9d5953864954e74e0a703ed0e84aa95f643a4b0
Charles Canan <ccanan1@lsu.edu>
2019-03-21 11:51:25 -0500

    update

---
+++ map1/webmap1.js
@@ -0,0 +1,15 @@
+let myMap = L.map('map1').setView([29.231155, -89.999163], 12)
+//L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(myMap)
+var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+let gifd = L.marker([29.231155, -89.999163]).addTo(myMap)
+let myPolygon = L.polygon([
+  [29.260670, -89.964004],
+  [29.250728, -89.957063],
+  [29.198470, -90.040388],
+  [29.209122, -90.048133]
+]).addTo(myMap);
+gifd.bindPopup('<em>GIFD </em>')
+myPolygon.bindPopup('GIFD service area')
